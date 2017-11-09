4fa2e79394713b38abba1e7c76770f341bcef7e6
Adam Parrish <aparrish@neosavvy.com>
2017-11-09 14:47:04 -0500

    ## API Definition
    - Breaking up the API into 3 layers
    - Defining the API / REST layer
    - Defining the Service Layer for mediating with Data Access
    - Stubbing in a few calls from API to Service layer
    - Verifying it works in PostMan
    - Sample PostMan collection.

---
+++ backend-node/src/user_service.js
@@ -0,0 +1,14 @@
+import {
+    findUserById,
+    findUsersForIds,
+    insertUser,
+} from './user_data'
+
+export const addUser = function (user) {
+    return -1
+};
+
+export const findAllUsers = () => {
+
+};
+
