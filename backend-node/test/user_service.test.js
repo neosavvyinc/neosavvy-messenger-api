362974a5d29e3ffe3ca9495736fbab7a94606819
Adam Parrish <aparrish@neosavvy.com>
2017-11-10 21:38:15 -0500

    Fixing broken Tests
    ### Async / Await - Breaks Sync Tests
    
    - Tests were written synchronously originally
    - After integrating with MySQL / PostMan all tests broke due to async/await
    - Making tests hang around to ensure code "stays working"

--- backend-node/test/user_service.test.js
+++ backend-node/test/user_service.test.js
@@ -11,50 +11,76 @@ import sinon from 'sinon';
 
 describe('addUser', function() {
 
-    it('should call the insert user function with params', function() {
-        const stub = sinon.stub(UserDataAccess, 'insertUser').callsFake((user) => {
-            return _.merge({id: 42}, user);
+    describe('success conditions', function() {
+        let stubUserDataService;
+        beforeEach(function() {
+            stubUserDataService = {
+                insertUser: sinon.stub(UserDataAccess, 'insertUser').callsFake((user) => {
+                    const fakeResult = _.merge({id: 42}, user);
+                    const promise = new Promise(
+                        success => {
+                            success(fakeResult);
+                        },
+                        error => {
+                            error(fakeResult)
+                        });
+                    return promise;
+                })
+            };
         });
-        const sampleUser = { username: 'test', password: 'pass' };
-        const result = UserService.addUser(sampleUser);
-        assert(UserDataAccess.insertUser.calledOnce);
-        expect(result).to.equal(42);
-        stub.restore();
-    });
 
-    it('should call the findUserByUsername', function() {
-        const spy = sinon.spy(UserDataAccess, 'findUserByUsername');
-        const stub = sinon.stub(UserDataAccess, 'insertUser').callsFake((user) => {
-            return _.merge({id: 42}, user);
+
+        it('should call the insert user function with params', async function() {
+            const stub = stubUserDataService.insertUser;
+            const sampleUser = { username: 'test', password: 'pass' };
+            const result = await UserService.addUser(sampleUser);
+            assert(UserDataAccess.insertUser.calledOnce);
+            expect(result).to.equal(42);
+            stub.restore();
         });
-        UserService.addUser({ username: 'test', password: 'pass' });
-        assert(UserDataAccess.findUserByUsername.calledOnce);
-        spy.restore();
-        stub.restore();
+
+        it('should call the findUserByUsername function', async function() {
+            const spy = sinon.spy(UserDataAccess, 'findUserByUsername');
+            const stub = stubUserDataService.insertUser;
+            await UserService.addUser({ username: 'test', password: 'pass' });
+            assert(UserDataAccess.findUserByUsername.calledOnce);
+            spy.reset();
+            spy.restore();
+            stub.restore();
+        });
     });
 
     describe('error conditions', function() {
 
-        it('should throw an exception when user name is empty', function() {
-            const addUser = () => {
-                UserService.addUser({})
+        let errorSpy, addUser;
+        beforeEach(function() {
+            errorSpy = sinon.spy();
+            addUser = async (fakeUser) =>  {
+                try {
+                    await UserService.addUser(fakeUser).catch(errorSpy);
+                } catch (e) {
+                    console.log('throwing error: ', e.message);
+                    throw new Error(e.message);
+                }
             };
-            expect(addUser).to.throw('Username was not provided, please provide a username')
         });
 
-        it('should throw an exception when user name is set but password empty', function() {
-            const addUser = () => {
-                UserService.addUser({ username: 'test'})
-            };
-            expect(addUser).to.throw('Password was not provided, please provide a password')
+        it('should throw an exception when user name is empty', async function() {
+            await addUser({});
+            expect(errorSpy.callCount).to.equal(1);
+            expect(errorSpy.args[0][0].message).to.equal('Username was not provided, please provide a username')
         });
 
-        it('should throw an error when a username is found for the one provided', function() {
+        it('should throw an exception when user name is set but password empty', async function() {
+            await addUser({ username: 'test'});
+            expect(errorSpy.callCount).to.equal(1);
+            expect(errorSpy.args[0][0].message).to.equal('Password was not provided, please provide a password')
+        });
+
+        it('should throw an error when a username is found for the one provided', async function() {
             const stub = sinon.stub(UserDataAccess, 'findUserByUsername').callsFake((user) => { return user });
-            const addUser = () => {
-                UserService.addUser({ username: 'nonunique', password: 'test'})
-            };
-            expect(addUser).to.throw('Please provide a unique username');
+            await addUser({ username: 'nonunique', password: 'test'});
+            expect(errorSpy.args[0][0].message).to.equal('Please provide a unique username');
             stub.restore();
         });
     });
